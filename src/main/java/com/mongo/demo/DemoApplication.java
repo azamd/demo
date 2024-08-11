951f70adb7c333f58542561d7ca20feccd54b533
azamd <tinkertailor1999@gmail.com>
2024-08-11 23:24:41 +0100

    remove unused code section

--- src/main/java/com/mongo/demo/DemoApplication.java
+++ src/main/java/com/mongo/demo/DemoApplication.java
@@ -30,16 +30,6 @@ public class DemoApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		/*String uri = "mongodb+srv://aziz:aziz@aziz-mongodb-cluster.mrdntek.mongodb.net/?retryWrites=true&w=majority&appName=Aziz-MongoDB-Cluster";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("orders");
-            MongoCollection<Document> collection = database.getCollection("purchases");
-            Document doc = collection.find(eq("purchase","electronics")).first();
-            if (doc != null) {
-                System.out.println(doc.toJson());
-            } else {
-                System.out.println("No matching documents found.");
-            }*/
 	}
 
 	@Override
